7431d40b1f0417512278347d05bc08fffd23c47b
Sergei Kolisni4enko <skolisni4enko@gmail.com>
2022-01-30 11:48:55 +0200

    First

---
+++ src/main/java/sample/Constants.java
@@ -0,0 +1,117 @@
+package sample;
+
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Constants {
+    List<String> material = new ArrayList<String>(); //знание и восприятие материала на уроке
+    private List<String> homeWork = new ArrayList<String>(); //выполнение домашних заданий
+    private List<String> generalCharacter = new ArrayList<String>(); //основные характеристики студента
+    private List<String> selfEducation = new ArrayList<String>(); //работа над саморазвитием
+    private List<String> goodWork = new ArrayList<String>(); //студент немного недорабатывает
+    private List<String> badWork = new ArrayList<String>(); //студент плохо работает над ДЗ
+
+    String getMaterial() {
+        int a = (int) (Math.random() * material.size());
+        return material.get(a);
+    }
+
+    String getHomework() {
+        int a = (int) (Math.random() * homeWork.size());
+        return homeWork.get(a);
+    }
+
+    String getGeneralCharacter() {
+        int a = (int) (Math.random() * generalCharacter.size());
+        return generalCharacter.get(a);
+    }
+
+    String getSelfEducation() {
+        int a = (int) (Math.random() * selfEducation.size());
+        return selfEducation.get(a);
+    }
+
+    String getGoodWork() {
+        int a = (int) (Math.random() * goodWork.size());
+        return goodWork.get(a);
+    }
+
+    String getBadWork() {
+        int a = (int) (Math.random() * badWork.size());
+        return badWork.get(a);
+    }
+
+
+    void initializeLists() {
+        try {
+            FileReader fileReader = new FileReader("comments.txt");
+            BufferedReader scanner = new BufferedReader(fileReader);
+            String nextString;
+            while ((nextString = scanner.readLine()) != null) {
+
+                if (nextString.contains("material")) {
+                    nextString = scanner.readLine();
+                    while (!nextString.contains("homeWork")) {
+                        material.add(nextString);
+                        nextString = scanner.readLine();
+                    }
+                }
+
+                if (nextString.contains("homeWork")) {
+                    nextString = scanner.readLine();
+                    while (!nextString.contains("generalCharacter")) {
+                        homeWork.add(nextString);
+                        nextString = scanner.readLine();
+                    }
+                }
+
+                if (nextString.contains("generalCharacter")) {
+                    nextString = scanner.readLine();
+                    while (!nextString.contains("selfEducation")) {
+                        generalCharacter.add(nextString);
+                        nextString = scanner.readLine();
+                    }
+                }
+
+                if (nextString.contains("selfEducation")) {
+                    nextString = scanner.readLine();
+                    while (!nextString.contains("goodWork")) {
+                        selfEducation.add(nextString);
+                        nextString = scanner.readLine();
+                    }
+                }
+
+                if (nextString.contains("goodWork")) {
+                    nextString = scanner.readLine();
+                    while (!nextString.contains("badWork")) {
+                        goodWork.add(nextString);
+                        nextString = scanner.readLine();
+                    }
+                }
+
+                if (nextString.contains("badWork")) {
+                    nextString = scanner.readLine();
+                    while (nextString != null) {
+                        badWork.add(nextString);
+                        nextString = scanner.readLine();
+                    }
+                }
+
+            }
+            scanner.close();
+            fileReader.close();
+        } catch (FileNotFoundException e) {
+            JOptionPane.showMessageDialog(null, "File Not Found!");
+            e.printStackTrace();
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Some problem in FIle...");
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/sample/Controller.java
@@ -0,0 +1,91 @@
+package sample;
+
+import java.net.URL;
+import java.util.Objects;
+import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+    @FXML // fx:id="nameTextField"
+    private TextField nameTextField; // Value injected by FXMLLoader
+    @FXML // fx:id="commentTextArea"
+    private TextArea commentTextArea; // Value injected by FXMLLoader
+    @FXML // fx:id="levelChoiceBox"
+    private ChoiceBox<String> levelChoiceBox; // Value injected by FXMLLoader
+    @FXML // fx:id="btnGenerate"
+    private Button btnGenerate; // Value injected by FXMLLoader
+    @FXML
+    private Button btnCleanField;
+
+    Constants constants = new Constants();
+    ObservableList<String> marks = FXCollections.observableArrayList("Excellent", "Good", "Bad");
+    private boolean isInitialize = false;
+
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        //Кнопка генерации
+        btnGenerate.setOnAction(event -> {
+            if (!isInitialize) {
+                constants.initializeLists();
+                isInitialize = true;
+            }
+
+            String comment = null;
+
+            if (Objects.equals(levelChoiceBox.getValue(), "Excellent"))
+            {
+                comment = nameTextField.getText() + " "
+                        + constants.getMaterial() + " "
+                        + constants.getHomework() + " "
+                        + constants.getGeneralCharacter() + " "
+                        + constants.getSelfEducation();
+            }
+
+            if (Objects.equals(levelChoiceBox.getValue(),"Good"))
+            {
+                comment = nameTextField.getText() + " "
+                        + constants.getMaterial() + " "
+                        + constants.getBadWork() + " "
+                        + constants.getGeneralCharacter() + " "
+                        + constants.getGoodWork();
+            }
+
+            if (Objects.equals(levelChoiceBox.getValue(),"Bad"))
+            {
+                comment = nameTextField.getText() + " "
+                        + constants.getMaterial() + " "
+                        + constants.getGeneralCharacter() + " "
+                        + constants.getBadWork();
+            }
+
+            if (Objects.equals(levelChoiceBox.getValue(),null))
+            {
+                comment = "Select student level!";
+            }
+
+            commentTextArea.setText(comment);
+        });
+        // Очистка полей
+        btnCleanField.setOnAction(event -> {
+            nameTextField.clear();
+            commentTextArea.clear();
+        });
+
+        levelChoiceBox.setItems(marks);
+    }
+
+
+}
